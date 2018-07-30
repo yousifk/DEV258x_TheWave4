10f9cc6ef178191e42af2b195db982c87d180a21
YousifK <yousifk55@gmail.com>
2018-07-30 10:19:37 +0300

    SQL ADD

---
+++ Entities/MoviesContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MovieApp.Entities
+{
+    public partial class MoviesContext : DbContext
+    {
+        public MoviesContext()
+        {
+        }
+
+        public MoviesContext(DbContextOptions<MoviesContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySql("server=127.0.0.1;userid=root;pwd=admin;port=3306;database=Movies;sslmode=none;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+    }
+}
