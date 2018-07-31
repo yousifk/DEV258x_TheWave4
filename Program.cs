e029740ecf12b03527be6e978f20a9be98e25c0b
YousifK <yousifk55@gmail.com>
2018-07-31 12:47:12 +0300

    run CreateItem

--- Program.cs
+++ Program.cs
@@ -2,13 +2,12 @@
 using System.Linq;
 using MovieApp.Entities;
 
-namespace MovieApp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Module1Helper.SelectById();
+namespace MovieApp {
+    class Program {
+        static void Main (string[] args) {
+            // Module1Helper.SelectById();
+            Module1Helper.CreateItem ();
+
         }
     }
 }
